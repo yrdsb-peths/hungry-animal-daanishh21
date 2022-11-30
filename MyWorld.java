a3f21121d79ea1141cdfe11cae30a239a438becf
daanishh21 <117941519+daanishh21@users.noreply.github.com>
2022-11-30 14:23:52 -0500

    added elephant constructor

--- MyWorld.java
+++ MyWorld.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Elephant the Hero
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author daanish
+ * @version November 2022
  */
 public class MyWorld extends World
 {
@@ -13,6 +13,12 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+     
+     super(600, 400, 1);
+     
+     Elephant elephant = new Elephant();
+     addObject(elelphant, 300, 200);
+     
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
